a8d976e12fdf52bc179c3dd037daf8c04e0f5f67
Cody Schwengler01~ <cschwengler@outlook.com>
2023-12-17 19:12:58 -0800

    made them into seperate things

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,8 @@ const typeDefs = `
         email: String
         bookCount: Int
         savedBooks: [Book]
-    },
+    }
+
     type Book {
         bookId: String
         authors: [String]
@@ -13,18 +14,23 @@ const typeDefs = `
         title: String
         image: String
         link: String
-    },
+    }
+
     type Auth {
         token: ID!
         user: User
-    },
+    }
+
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
         saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
         removeBook(bookId: String): User
-    },
+    }
+
     type Query {
         me: User
     }
-`
+`;
+
+module.exports = typeDefs;
